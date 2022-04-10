a5c130ded3c2b05a4251d8157466afbf01d2e73e
Cristian Vlasceanu <cvlasceanu@>
2022-04-10 16:06:21 -0700

    AssignProcessToJobObject, fixed cleanup code

--- sandbox.cpp
+++ sandbox.cpp
@@ -4,15 +4,51 @@
 #include <sstream>
 
 
-void execute(const std::wstring& CmdLine)
+static void assign_process_to_job(HANDLE hJob, HANDLE hProcess)
+{
+    JOBOBJECT_EXTENDED_LIMIT_INFORMATION jeli = {};
+    jeli.BasicLimitInformation.LimitFlags =
+          JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+        | JOB_OBJECT_LIMIT_SCHEDULING_CLASS;
+
+    CALL_API(::SetInformationJobObject(hJob, JobObjectExtendedLimitInformation, &jeli, sizeof jeli));
+
+    JOBOBJECT_BASIC_UI_RESTRICTIONS ui_restrict = {
+          JOB_OBJECT_UILIMIT_DESKTOP
+        | JOB_OBJECT_UILIMIT_DISPLAYSETTINGS
+        | JOB_OBJECT_UILIMIT_GLOBALATOMS
+        | JOB_OBJECT_UILIMIT_HANDLES
+        | JOB_OBJECT_UILIMIT_SYSTEMPARAMETERS
+        | JOB_OBJECT_UILIMIT_READCLIPBOARD
+        | JOB_OBJECT_UILIMIT_WRITECLIPBOARD
+    };
+    CALL_API(::SetInformationJobObject(hJob, JobObjectBasicUIRestrictions, &ui_restrict, sizeof ui_restrict));
+
+    CALL_API(::AssignProcessToJobObject(hJob, hProcess));
+}
+
+
+static void execute(const std::wstring& CmdLine)
 {
     if (!CmdLine.empty())
     {
+        HANDLE hJob = ::CreateJobObject(nullptr, nullptr);
+        if (!hJob)
+            ThrowError("CreateJobObject");
+
         RestrictedTokenBuilder tokenBuilder;
 
         STARTUPINFOW si = {};
         PROCESS_INFORMATION pi = {};
         DWORD Flags = 0;
+
+        OnScopeExit<> cleanup([&]() {
+            if (pi.hThread)
+                ::CloseHandle(pi.hThread);
+            if (pi.hProcess)
+                ::CloseHandle(pi.hProcess);
+            ::CloseHandle(hJob);
+        });
 
         CALL_API(::CreateProcessAsUserW(
             tokenBuilder.get_token(),
@@ -28,13 +64,13 @@ void execute(const std::wstring& CmdLine)
             &pi             // Pointer to PROCESS_INFORMATION structure
         ));
 
-         // Wait until child process exits.
-        CALL_API(::WaitForSingleObject(pi.hProcess, INFINITE));
+        assign_process_to_job(hJob, pi.hProcess);
 
-        ::CloseHandle(pi.hThread);
-        ::CloseHandle(pi.hProcess);
+        // Wait until child process exits.
+        CALL_API(::WaitForSingleObject(pi.hProcess, INFINITE));
     }
 }
+
 
 int wmain(int argc, const wchar_t* argv[])
 {
